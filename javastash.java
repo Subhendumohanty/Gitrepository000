938eaaeb987a9da28b9a3e60ba180946ecbe1b07
Your Name <you@example.com>
2022-05-30 16:39:20 +0530

    manager work

--- javastash.java
+++ javastash.java
@@ -1,1 +1,3 @@
 This is my java file yah do java file you can save this now you have stape at point of time.
+this is imporant work for manager find out manager work to  do something.
+separete the work
