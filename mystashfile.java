dd1fad7d2c9e7d9f344a40727315d29015e3ab5a
Your Name <you@example.com>
2022-05-31 11:29:23 +0530

    my stash file work

--- mystashfile.java
+++ mystashfile.java
@@ -4,9 +4,11 @@ suddenly our manager assign a emergancy task file what will we do.
 1st line by devloper 
 2nd line by devloper
 3nd line by developer
+Updated upstream
 my manager work 
 1st commut
 2nd commit 
-3 ed commit
+3 ed commi
 4ht commit
+4ht commit 90% work done Stashed changes
 
