01b34d6d210b62d7ca23683c3603e4ec89b061e0
Your Name <you@example.com>
2022-05-31 11:09:45 +0530

    my stash file

--- javastash.java
+++
@@ -1,8 +0,0 @@
-This is my java file yah do java file you can save this now you have stape at point of time.
-this is imporant work for manager find out manager work to  do something.
-separete the work.
-1st line of dev work
-2nd line dev work
-3ed line dev work
-4th line dev work
-5th line dev work
---
+++ mystashfile.java
@@ -0,0 +1,2 @@
+THIS IS stash file it is a hidden file when we are complited 90% of work 
+suddenly our manager assign a emergancy task file what will we do.
