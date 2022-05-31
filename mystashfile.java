75b0091862bcac300ded25c0c62032b66ae6a35d
Your Name <you@example.com>
2022-05-31 11:21:35 +0530

    my 2nd commit

--- mystashfile.java
+++ mystashfile.java
@@ -2,5 +2,5 @@ iTHIS IS stash file it is a hidden file when we are complited 90% of work
 suddenly our manager assign a emergancy task file what will we do.
 
 1st line by devloper 
-
+2nd line by devloper
 
