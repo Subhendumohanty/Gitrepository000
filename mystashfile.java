a021a9cb2c48e26e084bda8b8533414fdc42744d
Your Name <you@example.com>
2022-05-31 11:20:46 +0530

    task one

--- mystashfile.java
+++ mystashfile.java
@@ -2,8 +2,5 @@ iTHIS IS stash file it is a hidden file when we are complited 90% of work
 suddenly our manager assign a emergancy task file what will we do.
 
 1st line by devloper 
-2nd line by devloper
-3ed line by dev
-4th lone by dev
-5th line by dev
+
 
